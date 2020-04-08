f3c1f08b7d336a74b2d9cd6d5ae1044b5c5cf6b5
Ahd <dafuq.ahd@gmail.com>
2020-04-08 20:14:11 +0300

    ready to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,25 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "file-loader",
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg)$/,
+        include: /src/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 999999999,
+            },
+          },
+        ],
+      },
     ],
   },
 };
