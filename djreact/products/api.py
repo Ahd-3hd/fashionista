29f333e6095d9f103abc820f3e55bf6e1736b72c
Ahd <dafuq.ahd@gmail.com>
2020-04-08 16:32:02 +0300

    remove foreign key and allow everyone to view products

--- djreact/products/api.py
+++ djreact/products/api.py
@@ -5,13 +5,14 @@ from .serializers import ProductSerializer
 
 # Product Viewset
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
     permission_classes = [
-        permissions.IsAuthenticated
+        permissions.AllowAny
     ]
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        return self.request.user.products.all()
+    # def get_queryset(self):
+    #     return self.request.user.products.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
